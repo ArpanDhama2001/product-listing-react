b7f0f44ac1d82655f511879277d4dba71ea0e6f4
Arpan Dhama <arpandhama2001@gmail.com>
2022-09-23 16:13:57 +0530

    minor fixes

--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -34,6 +34,7 @@ function ProductsContainer() {
 
   useEffect(() => {
     dispatch(fetchProducts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (status === STATUS.loading) {
